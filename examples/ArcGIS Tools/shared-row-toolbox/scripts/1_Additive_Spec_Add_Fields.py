cdd356c7abe70a705052288c6a6598a56ae3f5f7
David Wasserman <david.wasserman.plan@gmail.com>
2019-08-27 06:27:19 -0700

    Add Additive Fields Addition Tool
    Add a tool that adds all columns required for the additive field
    specification based on an updated CSV file derived from the
    specification sheet. Next is adding a tool that consolidates centerlines
    based on oneway attributes.
    Test tool on test data, keep test data ready for consolidated
    attributes.

---
+++ examples/ArcGIS Tools/shared-row-toolbox/scripts/1_Additive_Spec_Add_Fields.py
@@ -0,0 +1,101 @@
+# --------------------------------
+# Name: 1_Additive_Spec_Add_Fields.py
+# Purpose: This tool will add
+# Current Owner: David Wasserman
+# Last Modified: 8/25/2019
+# Copyright:   David Wasserman
+# ArcGIS Version:   ArcGIS Pro/10.4
+# Python Version:   3.5/2.7
+# --------------------------------
+# Copyright 2019 David J. Wasserman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# --------------------------------
+
+# Import Modules
+import arcpy
+import pandas as pd
+import os
+import sharedrowlib as srl
+
+
+
+def add_additive_specification_fields_by_table(in_fc, csv, number_of_expandable_lanes=4, field_name_col="Name",
+                                               type_col="Type", shp_field_name="Name_Shp", optional_col="Optional",
+                                               optional_bool=True, validate=False):
+    """This function will add additive shared-row specification fields based on a CSV. Uses Pandas.
+    :param - in_fc - the input feature class that will have all the shared-row fields added to it.
+    :param - csv - csv with field specification fields defined
+    :param - number_of_expandable_lanes - while the minimum number of expandable lanes (through lanes) is 4, this
+    parameter will enable the addition of more through lanes. This parameter does nothing if optional fields are not
+    added
+    :param - field_name_col - column of csv with field names being added
+    :param - type_col - column of csv with the field type being added
+    :param - shp_field_name - column of csv with field name to use if the file is a shapefile
+    :param - optional_col - column of csv with a column identifying optional fields with a 1 and
+    required fields as a 0
+    :param - optional_bool - if true, optional fields will be added
+    :param - validate - if true, all fields will go through a validate field routine
+    """
+    try:
+        arcpy.env.overwriteOutput = True
+        workspace = os.path.dirname(in_fc)
+        srl.arc_print(csv)
+        df = pd.read_csv(csv)
+        for index, row in df.iterrows():
+            raw_field = str(row[field_name_col])
+            if validate:
+                fieldname = arcpy.ValidateFieldName(raw_field, workspace)
+            else:
+                fieldname = raw_field
+            if shp_field_name and ".shp" in in_fc:
+                fieldname = str(row[shp_field_name])
+            if not optional_bool:
+                if row[optional_col] == 1:
+                    print("Not adding optional field.")
+                    continue
+            if fieldname[-1] == "X":
+                if number_of_expandable_lanes > 4:
+                    for i in range(5, int(number_of_expandable_lanes) + 1):
+                        new_numeric_field = fieldname.replace("_X", "_{0}".format(i))
+                        srl.add_new_field(in_fc, new_numeric_field, row[type_col])
+                    continue # Do not add X Field. 
+                else:
+                    continue
+            srl.add_new_field(in_fc, fieldname, row[type_col])
+        srl.arc_print("Script Complete!")
+    except Exception as e:
+        srl.arc_print("Tool Script Error!")
+        import traceback, sys
+        tb = sys.exc_info()[2]
+        srl.arc_print(e.args[0])
+        arcpy.AddError("The error occurred on line {0}...".format(tb.tb_lineno))
+
+
+# End do_analysis function
+
+# This test allows the script to be used from the operating
+# system command prompt (stand-alone), in a Python IDE,
+# as a geoprocessing script tool, or as a module imported in
+# another script
+if __name__ == '__main__':
+    # Define input parameters
+    import os
+
+    input_feature_class = arcpy.GetParameterAsText(0)
+    number_of_expandable_lanes = int(arcpy.GetParameterAsText(1))
+    optional_bool = bool(arcpy.GetParameterAsText(2))
+    specification_data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),"specification_data")
+    additive_csv = os.path.join(specification_data_dir,r"additive_shared_row_fields.csv")
+    add_additive_specification_fields_by_table(input_feature_class, additive_csv, number_of_expandable_lanes,
+                                               optional_bool=optional_bool)
---
+++ examples/ArcGIS Tools/shared-row-toolbox/scripts/sharedrowlib.py
@@ -0,0 +1,194 @@
+# --------------------------------
+# Name: sharedrowlib.py
+# Purpose: This file serves as a function library for the shared-row toolset for ArcGIS. Import as srl.
+# Current Owner: David Wasserman
+# Last Modified: 8/25/2019
+# Copyright:   David Wasserman
+# ArcGIS Version:   ArcGIS Pro/10.6
+# Python Version:   3.5/2.7
+# --------------------------------
+# Copyright 2019 David J. Wasserman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# --------------------------------
+
+# Import Modules
+import arcpy
+import numpy as np
+import os, re
+import datetime
+
+try:
+    import pandas as pd
+except:
+    arcpy.AddError("This library requires Pandas installed in the ArcGIS Python Install."
+                   " Might require installing pre-requisite libraries and software.")
+
+
+# Function Definitions
+def func_report(function=None, reportBool=False):
+    """This decorator function is designed to be used as a wrapper with other functions to enable basic try and except
+     reporting (if function fails it will report the name of the function that failed and its arguments. If a report
+      boolean is true the function will report inputs and outputs of a function.-David Wasserman"""
+
+    def func_report_decorator(function):
+        def func_wrapper(*args, **kwargs):
+            try:
+                func_result = function(*args, **kwargs)
+                if reportBool:
+                    print("Function:{0}".format(str(function.__name__)))
+                    print("     Input(s):{0}".format(str(args)))
+                    print("     Ouput(s):{0}".format(str(func_result)))
+                return func_result
+            except Exception as e:
+                print(
+                    "{0} - function failed -|- Function arguments were:{1}.".format(str(function.__name__), str(args)))
+                print(e.args[0])
+
+        return func_wrapper
+
+    if not function:  # User passed in a bool argument
+        def waiting_for_function(function):
+            return func_report_decorator(function)
+
+        return waiting_for_function
+    else:
+        return func_report_decorator(function)
+
+
+def arc_tool_report(function=None, arcToolMessageBool=False, arcProgressorBool=False):
+    """This decorator function is designed to be used as a wrapper with other GIS functions to enable basic try and except
+     reporting (if function fails it will report the name of the function that failed and its arguments. If a report
+      boolean is true the function will report inputs and outputs of a function.-David Wasserman"""
+
+    def arc_tool_report_decorator(function):
+        def func_wrapper(*args, **kwargs):
+            try:
+                func_result = function(*args, **kwargs)
+                if arcToolMessageBool:
+                    arcpy.AddMessage("Function:{0}".format(str(function.__name__)))
+                    arcpy.AddMessage("     Input(s):{0}".format(str(args)))
+                    arcpy.AddMessage("     Ouput(s):{0}".format(str(func_result)))
+                if arcProgressorBool:
+                    arcpy.SetProgressorLabel("Function:{0}".format(str(function.__name__)))
+                    arcpy.SetProgressorLabel("     Input(s):{0}".format(str(args)))
+                    arcpy.SetProgressorLabel("     Ouput(s):{0}".format(str(func_result)))
+                return func_result
+            except Exception as e:
+                arcpy.AddMessage(
+                    "{0} - function failed -|- Function arguments were:{1}.".format(str(function.__name__),
+                                                                                    str(args)))
+                print(
+                    "{0} - function failed -|- Function arguments were:{1}.".format(str(function.__name__), str(args)))
+                print(e.args[0])
+
+        return func_wrapper
+
+    if not function:  # User passed in a bool argument
+        def waiting_for_function(function):
+            return arc_tool_report_decorator(function)
+
+        return waiting_for_function
+    else:
+        return arc_tool_report_decorator(function)
+
+
+@arc_tool_report
+def arc_print(string, progressor_Bool=False):
+    """ This function is used to simplify using arcpy reporting for tool creation,if progressor bool is true it will
+    create a tool label."""
+    casted_string = str(string)
+    if progressor_Bool:
+        arcpy.SetProgressorLabel(casted_string)
+        arcpy.AddMessage(casted_string)
+        print(casted_string)
+    else:
+        arcpy.AddMessage(casted_string)
+        print(casted_string)
+
+
+@arc_tool_report
+def field_exist(featureclass, fieldname):
+    """ArcFunction
+     Check if a field in a feature class field exists and return true it does, false if not.- David Wasserman"""
+    fieldList = arcpy.ListFields(featureclass, fieldname)
+    fieldCount = len(fieldList)
+    if (fieldCount >= 1) and fieldname.strip():  # If there is one or more of this field return true
+        return True
+    else:
+        return False
+
+
+@arc_tool_report
+def add_new_field(in_table, field_name, field_type, field_precision="#", field_scale="#", field_length="#",
+                  field_alias="#", field_is_nullable="#", field_is_required="#", field_domain="#"):
+    """ArcFunction
+    Add a new field if it currently does not exist. Add field alone is slower than checking first.- David Wasserman"""
+    if field_exist(in_table, field_name):
+        print(field_name + " Exists")
+        arcpy.AddMessage(field_name + " Exists")
+    else:
+        print("Adding " + field_name)
+        arcpy.AddMessage("Adding " + field_name)
+        arcpy.AddField_management(in_table, field_name, field_type, field_precision, field_scale,
+                                  field_length,
+                                  field_alias,
+                                  field_is_nullable, field_is_required, field_domain)
+
+
+@arc_tool_report
+def validate_df_names(dataframe, output_feature_class_workspace):
+    """Returns pandas dataframe with all col names renamed to be valid arcgis table names."""
+    new_name_list = []
+    old_names = dataframe.columns.names
+    for name in old_names:
+        new_name = arcpy.ValidateFieldName(name, output_feature_class_workspace)
+        new_name_list.append(new_name)
+    rename_dict = {i: j for i, j in zip(old_names, new_name_list)}
+    dataframe.rename(index=str, columns=rename_dict)
+    return dataframe
+
+
+@arc_tool_report
+def arcgis_table_to_dataframe(in_fc, input_fields, query="", skip_nulls=False, null_values=None):
+    """Function will convert an arcgis table into a pandas dataframe with an object ID index, and the selected
+    input fields."""
+    OIDFieldName = arcpy.Describe(in_fc).OIDFieldName
+    final_fields = [OIDFieldName] + input_fields
+    np_array = arcpy.da.TableToNumPyArray(in_fc, final_fields, query, skip_nulls, null_values)
+    object_id_index = np_array[OIDFieldName]
+    fc_dataframe = pd.DataFrame(np_array, index=object_id_index, columns=input_fields)
+    return fc_dataframe
+
+@arc_tool_report
+def arcgis_table_to_df(in_fc,input_fields,query=""):
+    """Function will convert an arcgis table into a pandas dataframe with an object ID index, and the selected
+        input fields. Uses a da search cursor."""
+    OIDFieldName = arcpy.Describe(in_fc).OIDFieldName
+    final_fields = [OIDFieldName] + input_fields
+    record_list = [row for row in arcpy.da.SearchCursor(in_fc,final_fields,query)]
+    oid_collection = [row[0] for row in record_list]
+    fc_dataframe = pd.DataFrame(record_list, index=oid_collection, columns=final_fields)
+    return fc_dataframe
+
+
+
+# End do_analysis function
+
+# This test allows the script to be used from the operating
+# system command prompt (stand-alone), in a Python IDE,
+# as a geoprocessing script tool, or as a module imported in
+# another script
+if __name__ == '__main__':
+    # Define input parameters
+    print("Function library: sharedrowlib.py")
