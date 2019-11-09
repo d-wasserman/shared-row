0e959624e771b1137b47d27772382c661d1ef5dc
David Wasserman <david.wasserman.plan@gmail.com>
2019-11-09 11:17:16 -0800

    Update Dataframe methods
    Update dataframe methods to have beter docstrings and defaults.

--- examples/ArcGIS Tools/shared-row-toolbox/scripts/sharedrowlib.py
+++ examples/ArcGIS Tools/shared-row-toolbox/scripts/sharedrowlib.py
@@ -188,26 +188,35 @@ def retrieve_row_values(row, field_names, index_dict):
 
 
 @arc_tool_report
+def arcgis_table_to_df(in_fc, input_fields=None, query=""):
+    """Function will convert an arcgis table into a pandas dataframe with an object ID index, and the selected
+    input fields using an arcpy.da.SearchCursor.
+    :param - in_fc - input feature class or table to convert
+    :param - input_fields - fields to input to a da search cursor for retrieval
+    :param - query - sql query to grab appropriate values
+    :returns - pandas.DataFrame"""
+    OIDFieldName = arcpy.Describe(in_fc).OIDFieldName
+    if input_fields:
+        final_fields = [OIDFieldName] + input_fields
+    else:
+        final_fields = [field.name for field in arcpy.ListFields(in_fc)]
+    data = [row for row in arcpy.da.SearchCursor(in_fc,final_fields,where_clause=query)]
+    fc_dataframe = pd.DataFrame(data,columns=final_fields)
+    fc_dataframe = fc_dataframe.set_index(OIDFieldName,drop=True)
+    return fc_dataframe
+
+@arc_tool_report
 def arcgis_table_to_dataframe(in_fc, input_fields, query="", skip_nulls=False, null_values=None):
     """Function will convert an arcgis table into a pandas dataframe with an object ID index, and the selected
-    input fields."""
+    input fields. Uses TableToNumPyArray to get initial data."""
     OIDFieldName = arcpy.Describe(in_fc).OIDFieldName
-    final_fields = [OIDFieldName] + input_fields
+    if input_fields:
+        final_fields = [OIDFieldName] + input_fields
+    else:
+        final_fields = [field.name for field in arcpy.ListFields(in_fc)]
     np_array = arcpy.da.TableToNumPyArray(in_fc, final_fields, query, skip_nulls, null_values)
     object_id_index = np_array[OIDFieldName]
     fc_dataframe = pd.DataFrame(np_array, index=object_id_index, columns=input_fields)
-    return fc_dataframe
-
-
-@arc_tool_report
-def arcgis_table_to_df(in_fc, input_fields, query=""):
-    """Function will convert an arcgis table into a pandas dataframe with an object ID index, and the selected
-        input fields. Uses a da search cursor."""
-    OIDFieldName = arcpy.Describe(in_fc).OIDFieldName
-    final_fields = [OIDFieldName] + input_fields
-    record_list = [row for row in arcpy.da.SearchCursor(in_fc, final_fields, query)]
-    oid_collection = [row[0] for row in record_list]
-    fc_dataframe = pd.DataFrame(record_list, index=oid_collection, columns=final_fields)
     return fc_dataframe
 
 
